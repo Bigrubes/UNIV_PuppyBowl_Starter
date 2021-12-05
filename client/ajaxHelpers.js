ca8114d41fad3a5b3b01d65c46324d16c20e8c89
Reuben Ramos <rsramos23@gmail.com>
2021-12-05 09:01:20 -0800

    Completed Puppy Bowl Project

--- client/ajaxHelpers.js
+++ client/ajaxHelpers.js
@@ -1,15 +1,31 @@
 // Add your cohort name to the cohortName variable below, replacing the 'COHORT-NAME' placeholder
-const cohortName = 'COHORT-NAME';
+const cohortName = '2110-ftb-web-pt';
 // Use the APIURL variable for fetch requests
 const APIURL = `https://fsa-puppy-bowl.herokuapp.com/api/${cohortName}/`;
 
 
 export const fetchAllPlayers = async () => {
-
+    try {
+        const response = await fetch(`${APIURL}/players`);
+        const result = await response.json();
+        if (result.error) throw result.error;
+        return result.data.players;
+      } catch (err) {
+        console.error('Uh oh, trouble fetching players!', err);
+      }
 };
 
 export const fetchSinglePlayer = async (playerId) => {
-
+    try{
+    const response = await fetch(`${APIURL}/players/${playerId}`,{
+    });
+    const result = await response.json();
+    if (result.error) throw result.error;
+    console.log(result)
+    return result.data.player;
+    } catch(err){
+    console.error('Uh oh, trouble fetching players!', err);
+    }
 };
 
 export const addNewPlayer = async (playerObj) => {
@@ -17,5 +33,17 @@ export const addNewPlayer = async (playerObj) => {
 };
 
 export const removePlayer = async (playerId) => {
-
+  try {
+    const response = await fetch(`${APIURL}/players/${playerId}`, {
+    method: 'DELETE',
+    });
+    const result = await response.json();
+    if (result.error) throw result.error;
+    return;
+    } catch (err) {
+    console.error(
+    `Whoops, trouble removing player #${playerId} from the roster!`,
+    err
+    );
+  }
 };
